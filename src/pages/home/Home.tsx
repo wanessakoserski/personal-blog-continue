809e73b97888c34e9fafacf6bc562323925f7f62
Wanessa Koserski <wkoserski@gmail.com>
2022-07-27 17:58:16 -0300

    react introduction and configuration

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='title'>Home</h1>
+            <img src="https://www.oficinadanet.com.br/media/post/24969/750/kernel-panic-3.10.0-229.4.2.el7.png" alt="Linux"/>
+        </>
+    );
+}
+
+export default Home;
